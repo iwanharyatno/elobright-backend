45954984bb22cfdadf9511119f2941d186be52b6
Iwan Haryatno <iwnharry61@gmail.com>
2026-04-02 09:20:36 +0700

    fix(ci): mocked test envars.

--- src/config/env.ts
+++ src/config/env.ts
@@ -10,11 +10,17 @@ const envSchema = z.object({
     TIME_ZONE: z.string().default('Asia/Jakarta'),
 });
 
+const isTest = process.env.NODE_ENV === 'test';
 const _env = envSchema.safeParse(process.env);
 
-if (!_env.success) {
+if (!isTest && !_env.success) {
     console.error('Invalid environment variables', _env.error.format());
     process.exit(1);
 }
 
-export const env = _env.data;
+export const env = _env.success ? _env.data : {
+    PORT: '3000',
+    DATABASE_URL: 'postgres://dummy:dummy@localhost:5432/dummy',
+    JWT_SECRET: 'supersecretjwtkey12345',
+    TIME_ZONE: 'Asia/Jakarta'
+} as z.infer<typeof envSchema>;
